979874afe3d7db30f9659549a2738453882ff648
Antti Kaihola <antti.kaihola@eniram.fi>
2015-10-21 15:03:26 +0300

    Dropped _source and _editable from dep_cache keys

--- piptools/package_manager.py
+++ piptools/package_manager.py
@@ -206,18 +206,37 @@ class PersistentCache(object):
             self._cache = {'__format__': 1}
 
     def write_cache(self):
-        """Writes (pickles) the cache to disk."""
+        """Writes (pickles) the cache to disk.
+
+        Omits packages which come from a URL or have a *.dev version number,
+        since dependencies of such versions may change without the URL or
+        version string changing.
+
+        """
         with open(self._cache_file, 'wb') as f:
-            pickle.dump(self._cache, f)
+            cache_specs = ((key, value) for key, value in self._cache.items()
+                           if key != '__format__')
+            cache_without_repos = {
+                (name, version, url): value
+                for (name, version, url), value in cache_specs
+                if url is None and not version.endswith('.dev')}
+            cache_without_repos['__format__'] = self._cache['__format__']
+            pickle.dump(cache_without_repos, f)
+
+    def _get_cache_key(self, spec):
+        assert len(spec._preds) == 1
+        qualifier, version = next(iter(spec._preds))
+        assert qualifier == '=='
+        return spec._name, version, spec._url
 
     def __contains__(self, item):
-        return item in self.cache
+        return self._get_cache_key(item) in self.cache
 
     def __getitem__(self, key):
-        return self.cache[key]
+        return self.cache[self._get_cache_key(key)]
 
     def __setitem__(self, key, value):
-        self.cache[key] = value
+        self.cache[self._get_cache_key(key)] = value
         self.write_cache()
 
     def get(self, key, default=None):
